25ec1acf04ea9437536366956747971e56f5a412
Reemanshi <rchug@innow8apps.com>
2021-12-08 20:10:45 +0530

    updates

--- src/components/healthGoals/components/header/index.js
+++ src/components/healthGoals/components/header/index.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
-import {
-  Container,
-} from "reactstrap";
-import {itemArray, headerSliderSettings } from "../../utils";
+import React, { useState, useEffect } from "react";
+import { Container } from "reactstrap";
+import { itemArray, headerSliderSettings } from "../../utils";
 import Slider from "react-slick";
 
 const Header = ({ onClick }) => {
+  const [isDesktop, setIsDesktop] = useState(false);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const handleResize = () => {
+    if (window.innerWidth < 700) {
+      setIsDesktop(false);
+    } else {
+      setIsDesktop(true);
+    }
+  };
   const [activeItem, setActiveItem] = useState(itemArray[0].name);
   const onItemClick = (item) => {
     setActiveItem(item);
@@ -19,37 +31,57 @@ const Header = ({ onClick }) => {
             <span className="bold-logo">Health</span>
             <span className="normal-logo">Goals</span>
           </div>
-          {/* <Slider {...headerSliderSettings} className="header">
-            {itemArray.map((item) => {
-              return (
-                <div
-                  key={item.name}
-                  onClick={() => {
-                    onItemClick(item.name)
-                  } }
-                  className={`my-slick ${item.name === activeItem ? "active-item": ""}`}
-                >
-                  <img src={item.name === activeItem ? item.activeImg : item.inActiveImg} alt="icon" />
-                  <span className="menu-name">{item.name}</span>
-                </div>
-              );
-            })}
-          </Slider> */}
-          <div className="header-wrapper">
-          {itemArray.map((item)=>{
-            return (
-            <div className="header-menu">
-              <img src={item.name === activeItem ? item.activeImg : item.inActiveImg} alt="icon" />
-              <p className="menu-name">{item.name}</p>
+          {!isDesktop ? (
+            <Slider {...headerSliderSettings} className="header">
+              {itemArray.map((item) => {
+                return (
+                  <div
+                    key={item.name}
+                    onClick={() => {
+                      onItemClick(item.name);
+                    }}
+                    className={`my-slick ${
+                      item.name === activeItem ? "active-item" : ""
+                    }`}
+                  >
+                    <img
+                      src={
+                        item.name === activeItem
+                          ? item.activeImg
+                          : item.inActiveImg
+                      }
+                      alt="icon"
+                    />
+                    <span className="menu-name">{item.name}</span>
+                  </div>
+                );
+              })}
+            </Slider>
+          ) : (
+            <div className="header-wrapper">
+              {itemArray.map((item) => {
+                return (
+                  <div className="header-menu" onClick={() => {
+                    onItemClick(item.name);
+                  }}>
+                    <img
+                      src={
+                        item.name === activeItem
+                          ? item.activeImg
+                          : item.inActiveImg
+                      }
+                      alt="icon"
+                    />
+                    <p className="menu-name">{item.name}</p>
+                  </div>
+                );
+              })}
             </div>
-            )
-
-          })}
-          </div>
+          )}
         </div>
       </Container>
     </>
   );
-}
+};
 
 export default Header;
