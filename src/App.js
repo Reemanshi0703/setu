f9aca5abed237f6f3d86de65ec3a11bb525e9575
Reemanshi <rchug@innow8apps.com>
2021-12-08 11:31:05 +0530

    slider updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './assets/styles/main.scss';
-import NavigationBar from './components/NavigationBar';
-import CardComponent from './components/CardComponent';
+import "./assets/styles/main.scss";
+import NavigationBar from "./components/NavigationBar";
+import CardComponent from "./components/CardComponent";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
@@ -11,33 +10,82 @@ import "swiper/components/navigation/navigation.min.css";
 import "swiper/components/pagination/pagination.min.css";
 import { Container, Row } from "reactstrap";
 import * as data from "./data.json";
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 function App() {
-  const [ dataToDisplay , SetDataToDisplay ] = useState(data.default.eye);
-  const changeType = (type)=> {
-    SetDataToDisplay(data.default[type])
-  }
+  const [dataToDisplay, SetDataToDisplay] = useState(data.default.eye);
+  const changeType = (type) => {
+    SetDataToDisplay(data.default[type]);
+  };
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+
+  };
+
   return (
     <div className="App">
-      <NavigationBar changeType={changeType}/>
-         <Container>
+      <NavigationBar changeType={changeType} />
+      {/* <Container>
         <Row className="mt-5">
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        slidesPerGroup={3}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        navigation={true}
-        className="mySwiper">
-          {dataToDisplay.map((item)=>{
-            return <>
-            <SwiperSlide><CardComponent {...item}/></SwiperSlide>
-            </>
-          })}
-      </Swiper>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            slidesPerGroup={3}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            navigation={true}
+            className="mySwiper"
+          >
+            {dataToDisplay.map((item) => {
+              return (
+                <>
+                  <SwiperSlide>
+                    <CardComponent {...item} />
+                  </SwiperSlide>
+                </>
+              );
+            })}
+          </Swiper>
         </Row>
-       </Container>
+      </Container> */}
+     <div className="container">
+        <Slider {...settings}>
+        {dataToDisplay.map((item) => {
+              return (
+                    <CardComponent {...item} />
+              );
+            })}
+        </Slider>
+      </div>
     </div>
   );
 }
