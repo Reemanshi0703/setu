124266a91c90b668865f9a985e35455977fb573e
Reemanshi <rchug@innow8apps.com>
2021-12-08 14:29:35 +0530

    scroll functionality

--- src/components/healthGoals/components/header/index.js
+++ src/components/healthGoals/components/header/index.js
@@ -1,36 +1,60 @@
+import React, { useRef } from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+} from "reactstrap";
+import menuIcon from "../../../../assets/images/js-logo.png";
 
-import { Navbar, NavbarBrand, NavItem, NavLink, Nav, Container } from "reactstrap";
-import menuIcon from "../../../../assets/images/js-logo.png"
+const itemArray = ["tummy", "eye", "skin", "sleep", "weight"];
 
-function NavigationBar({changeType}) {
-  const itemArray = ["tummy","eye", "skin", "sleep", "weight" ]
+function NavigationBar({ changeType }) {
+  let scrollRefRecords = useRef({});
+  itemArray.forEach((item) => {
+    scrollRefRecords.current[item] = React.createRef();
+  });
+  const scroll = (item) => {
+    scrollRefRecords.current[item].current.scrollIntoView({ inline: "center" });
+  };
   return (
     <>
-      <Navbar
-        expand="md"
-        light
-      >
+      <Navbar expand="md" light>
         <Container className="menu-container">
           <div>
             <NavbarBrand to="/" className="navbar-brand">
-              <span className="bold-logo">Health</span> <span className="normal-logo">Goals</span>
+              <span className="bold-logo">Health</span>{" "}
+              <span className="normal-logo">Goals</span>
             </NavbarBrand>
           </div>
-            <div>
+          <div>
             <Nav className="me-auto" navbar>
-              {itemArray.map((item)=>{
-                return <>
-                <NavItem onClick={()=>{changeType(item)}}>
-                <NavLink to="/" className="active"><img src={menuIcon} alt="icon" /><span className="menu-name">{item}</span></NavLink>
-              </NavItem>
-                </>
+              {itemArray.map((item, index) => {
+                return (
+                  <div ref={scrollRefRecords.current[item]}>
+                    <NavItem
+                      id={`index_${index}`}
+                      onClick={() => {
+                        changeType(item);
+                        scroll(item);
+                      }}
+                    >
+                      <NavLink to="/" className="active">
+                        <img src={menuIcon} alt="icon" />
+                        <span className="menu-name">{item}</span>
+                      </NavLink>
+                    </NavItem>
+                  </div>
+                );
               })}
-            </Nav>  
-            </div>  
+            </Nav>
+          </div>
         </Container>
       </Navbar>
     </>
-  )
+  );
 }
 
 export default NavigationBar;
